c230fe5df74ce2c16f07c978aaa64df9f27ee2a0
Sadnesjke <xyilaxdolbaeb@mail.ru>
2026-06-17 10:28:54 +0800

    Fix router: use hash history

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 import PeoplePage from '../views/PeoplePage.vue'
 import BookingPage from '../views/BookingPage.vue'
 import NewEventPage from '../views/NewEventPage.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: '/', component: HomePage },
     { path: '/people', component: PeoplePage },
